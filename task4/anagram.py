a2d585af0f302159841fe45051977d5c6c1afaf9
kmohan778 <85980323+kmohan778@users.noreply.github.com>
2021-06-17 23:33:19 +0530

    Create anagram.py

---
+++ task4/anagram.py
@@ -0,0 +1,6 @@
+1 = input("Enter the first value: ")
+s2 = input("Enter the second value: ")
+if(sorted(s1)== sorted(s2)):
+  print("The strings are anagrams.")
+else:
+  print("The strings aren't anagrams.")
