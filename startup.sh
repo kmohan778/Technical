74a6adbc0c1cc5e7eb92fdcb706465a182b5467b
kmohan778 <85980323+kmohan778@users.noreply.github.com>
2021-06-17 18:04:45 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,4 +7,4 @@ apt-get install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-#docker-compose up -d 
+docker-compose up -d 
