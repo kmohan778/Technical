eee91aa323d9436ae4905825ed0961a1492656c6
kmohan778 <85980323+kmohan778@users.noreply.github.com>
2021-06-18 16:55:55 +0530

    Update repeat.py

--- task4/repeat.py
+++ task4/repeat.py
@@ -2,4 +2,4 @@ string = input("Enter the value: ")
 import collections
 results = collections.Counter(string)
 for k,v in results.items():
-    print(k,v,sep='', end='')
+  print(k,v,sep='', end='')
