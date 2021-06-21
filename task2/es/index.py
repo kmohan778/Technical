0262cd9fc1893965ca6fd9eaeff6b578b9a21715
kmohan778 <85980323+kmohan778@users.noreply.github.com>
2021-06-21 08:42:04 +0530

    Update index.py

--- task2/es/index.py
+++ task2/es/index.py
@@ -1,13 +1,12 @@
-ES_HOST = "search-******************.ap-northeast-1.es.amazonaws.com"
 ### Elasticsearch prefix for index name
 INDEX_PREFIX = "awslogs"
 
 #################################################
 ### ELB access log format keys
-ELB_KEYS = ["timestamp", "elb", "client_ip", "client_port", "backend_ip", "backend_port", "request_processing_time", "backend_processing_time", "response_processing_time", "elb_status_code", "backend_status_code", "received_bytes", "sent_bytes", "request_method", "request_url", "request_version", "user_agent"]
+S3_KEYS = ["timestamp", "elb", "client_ip", "client_port", "backend_ip", "backend_port", "request_processing_time", "backend_processing_time", "response_processing_time", "elb_status_code", "backend_status_code", "received_bytes", "sent_bytes", "request_method", "request_url", "request_version", "user_agent"]
 
 ### ELB access log format regex
-ELB_REGEX = '^(.[^ ]+) (.[^ ]+) (.[^ ]+):(\\d+) (.[^ ]+):(\\d+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (\\d+) (\\d+) \"(\\w+) (.[^ ]+) (.[^ ]+)\" \"(.+)\"'
+S3_REGEX = '^(.[^ ]+) (.[^ ]+) (.[^ ]+):(\\d+) (.[^ ]+):(\\d+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (.[^ ]+) (\\d+) (\\d+) \"(\\w+) (.[^ ]+) (.[^ ]+)\" \"(.+)\"'
 
 #################################################
 
@@ -21,7 +20,7 @@ from elasticsearch import helpers
 
 from time import time
 ES_HOST = os.environ.get('pubip')
-R = re.compile(ELB_REGEX)
+R = re.compile(S3_REGEX)
 INDEX = INDEX_PREFIX + "-" + datetime.strftime(datetime.now(), "%Y%m%d")
 URL = "http://" + ES_HOST + "/_bulk"
 
@@ -50,7 +49,7 @@ def handler(event, context):
         values = match.groups(0)
         if not elb_name:
             elb_name = values[1]
-        doc = dict(zip(ELB_KEYS, values))
+        doc = dict(zip(S3_KEYS, values))
 
         actions.append({"_index": INDEX, "_type": elb_name, "_source": doc})
 
